e578207ff49fc0ec096fbfce804bbe886fec832d
Ozmar Cota <ozmar.cota.@navix.io>
2025-09-11 17:14:56 -0600

    Adding EF Core to the project. Connecting PostgreSQL database. Creating first migration and implementing users repository. Adding register and login unit tests

---
+++ core-api/Repositories/Interfaces/IUsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using core_api.Models;
+
+namespace core_api.Repositories.Interfaces
+{
+    public interface IUsersRepository
+    {
+        Task<IList<User>> GetUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User> AddUserAsync(User user);
+    }
+}
---
+++ core-api/Repositories/UsersRepository.cs
@@ -0,0 +1,42 @@
+﻿using core_api.Models;
+using core_api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace core_api.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UsersRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<User>> GetUsersAsync()
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .ToListAsync();
+        }
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _context.Users
+                .FindAsync(id);
+        }
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> AddUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
